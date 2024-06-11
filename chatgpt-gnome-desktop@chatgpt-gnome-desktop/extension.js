3c30052a166268e7a5fe5b0690ea2287c40f1b56
HorrorPills <rafalmio90@gmail.com>
2024-06-11 10:26:48 +0200

    Added Wayland Support, Cookies, Popup Interactions, Fixes

--- chatgpt-gnome-desktop@chatgpt-gnome-desktop/extension.js
+++ chatgpt-gnome-desktop@chatgpt-gnome-desktop/extension.js
@@ -4,18 +4,21 @@ imports.gi.versions.WebKit2 = '4.1';
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const ExtensionUtils = imports.misc.extensionUtils;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
-const Webkit = imports.gi.WebKit2;
-const Gtk = imports.gi.Gtk;
+const Me = ExtensionUtils.getCurrentExtension();
 const Gio = imports.gi.Gio;
 const PopupMenu = imports.ui.popupMenu;
 
-
-let button, menu
-//the process that the window will open in
-let proc
+let button, menu, proc;
+let initialized = false;
 
 function init() {
+    if (initialized) {
+        log('Extension already initialized');
+        return;
+    }
+    initialized = true;
+
+    log('Initializing extension');
     button = new St.Bin({
         style_class: 'panel-button',
         reactive: true,
@@ -24,74 +27,85 @@ function init() {
         y_expand: false,
         track_hover: true
     });
-    //set menubar icon
+
     let gicon = Gio.icon_new_for_string(Me.path + "/icons/chatgpt_icon.png");
-    icon = new St.Icon({
-        gicon
-    });
+    let icon = new St.Icon({ gicon });
 
     button.set_child(icon);
-    button.connect('button-press-event', function(actor, event) {
+    button.connect('button-press-event', (actor, event) => {
+        log('Button pressed: ' + event.get_button());
         if (event.get_button() == 1) {
             toggleWindow();
         } else if (event.get_button() == 3) {
             toggleMenu();
         }
-    })
+    });
 
-    //create the menu
     menu = new PopupMenu.PopupMenu(button, 0.0, St.Side.TOP, 0);
     menu.actor.add_style_class_name('panel-status-menu-box');
     Main.uiGroup.add_actor(menu.actor);
     menu.actor.hide();
 
-    //create menu items
-    menuRestart = new PopupMenu.PopupMenuItem("Restart");
-    menuQuit = new PopupMenu.PopupMenuItem("Quit");
+    let menuRestart = new PopupMenu.PopupMenuItem("Restart");
+    let menuQuit = new PopupMenu.PopupMenuItem("Quit");
 
-    //add menu items to menu
     menu.addMenuItem(menuRestart);
     menu.addMenuItem(menuQuit);
 
-    //connect menu items to functions
-    menuRestart.connect('activate', function() {
-        reloadWindow();
-    }
-    );
-    menuQuit.connect('activate', function() {
-      killWindow();
-    });
-
+    menuRestart.connect('activate', reloadWindow);
+    menuQuit.connect('activate', killWindow);
 }
 
 function toggleWindow() {
-  if(!proc){
-    proc = Gio.Subprocess.new(
-      ['gjs', Me.path + '/window.js'],
-      Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
-    );
-    return;
-  }
-  //TODO: find a way to hide the window instead of killing it
-  killWindow();
+    log('Toggling window');
+    if (!proc) {
+        log('Creating new subprocess');
+
+        // Get the position of the button
+        let [x, y] = button.get_transformed_position();
+        let [width, height] = button.get_size();
+
+        proc = Gio.Subprocess.new(
+            ['gjs', Me.path + '/window.js', x.toString(), (y + height).toString()],
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+
+        proc.wait_async(null, (proc, res) => {
+            try {
+                proc.wait_finish(res);
+                log('Subprocess exited');
+            } catch (e) {
+                log('Subprocess wait failed: ' + e.message);
+            }
+            proc = null;
+        });
+
+        return;
+    }
+
+    killWindow();
 }
 
-function reloadWindow(){
-  if(!proc) return;
-  proc.force_exit();
-  proc = Gio.Subprocess.new(
-    ['gjs', Me.path + '/window.js'],
-    Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
-  );
+function reloadWindow() {
+    log('Reloading window');
+    killWindow();
+    toggleWindow();
 }
 
-function killWindow(){
-  if(!proc) return;
-  proc.force_exit();
-  proc = null;
+function killWindow() {
+    log('Killing window');
+    if (proc) {
+        try {
+            proc.force_exit();
+        } catch (e) {
+            log('Failed to kill subprocess: ' + e.message);
+        }
+        proc = null;
+    }
 }
 
 function toggleMenu() {
+    log('Toggling menu');
     if (menu.isOpen) {
         menu.close();
     } else {
@@ -99,16 +113,32 @@ function toggleMenu() {
     }
 }
 
-
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    log('Enabling extension');
+    if (!button.get_parent()) {
+        Main.panel._rightBox.insert_child_at_index(button, 0);
+    }
+    log('Button added to panel');
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    log('Disabling extension');
+    if (proc) {
+        killWindow();
+    }
+    if (button.get_parent()) {
+        Main.panel._rightBox.remove_child(button);
+    }
+    if (menu) {
+        menu.destroy();
+    }
+    log('Button removed from panel');
 }
 
 function main() {
     init();
     enable();
+    log('Extension initialized and enabled');
 }
+
+main();
--- chatgpt-gnome-desktop@chatgpt-gnome-desktop/window.js
+++ chatgpt-gnome-desktop@chatgpt-gnome-desktop/window.js
@@ -1,51 +1,59 @@
 imports.gi.versions.Gtk = '3.0';
 imports.gi.versions.WebKit2 = '4.1';
 
-const GLib = imports.gi.GLib;
-const Gtk = imports.gi.Gtk
-const Webkit = imports.gi.WebKit2;
+const { GLib, Gtk, WebKit2, Gdk } = imports.gi;
+
+function log(message) {
+    print('window.js: ' + message);
+}
+
+function prepareCookieStorage() {
+    const appName = 'ChatGPT-Gnome-Desktop-Extension';
+    const cookieFilename = 'cookies.sqlite';
+
+    const xdgDataHome = GLib.getenv('XDG_DATA_HOME') || GLib.build_filenamev([GLib.get_home_dir(), '.local', 'share']);
+    const appDataDir = GLib.build_filenamev([xdgDataHome, appName]);
+
+    log('Creating cookie storage directory: ' + appDataDir);
+    GLib.mkdir_with_parents(appDataDir, 0o700);
+    return GLib.build_filenamev([appDataDir, cookieFilename]);
+}
+
+function createWindow(x, y) {
+    log('Creating window');
+    const appWindow = new Gtk.Window({
+        type: Gtk.WindowType.POPUP,
+        default_width: 350,
+        default_height: 550,
+        title: 'ChatGPT'
+    });
+
+    appWindow.set_decorated(false);
+    appWindow.set_keep_above(true);
+
+    log(`Calculated position: x=${x}, y=${y}`);
+
+    appWindow.move(x, y);
+
+    const scrolledWindow = new Gtk.ScrolledWindow();
+    const cookieStorage = prepareCookieStorage();
+    const webContext = WebKit2.WebContext.get_default();
+    const cookieManager = webContext.get_cookie_manager();
+    cookieManager.set_persistent_storage(cookieStorage, WebKit2.CookiePersistentStorage.SQLITE);
+
+    const webView = new WebKit2.WebView({ web_context: webContext });
+    scrolledWindow.add(webView);
+    webView.load_uri('https://chat.openai.com/chat');
+
+    appWindow.add(scrolledWindow);
+    appWindow.connect('destroy', () => Gtk.main_quit());
+    appWindow.show_all();
+    log('Window created and shown at calculated position');
+}
 
 Gtk.init(null);
 
-function prepareCookieStorage() {
-  const appName = 'ChatGPT-Gnome-Desktop-Extension';
-  const cookieFilename = 'cookies.sqlite';
-
-  const xdgDataHome = GLib.getenv('XDG_DATA_HOME') || GLib.build_filenamev([GLib.get_home_dir(), '.local', 'share']);
-  const appDataDir = GLib.build_filenamev([xdgDataHome, appName]);
-
-  // Create the appDataDir if it doesn't exist
-  GLib.mkdir_with_parents(appDataDir, 0o700);
-  return GLib.build_filenamev([appDataDir, cookieFilename]);
-}
-
-function createWindow(){
-  const Window = new Gtk.Window({
-    type: Gtk.WindowType.TOPLEVEL
-  });
-  Window.set_position(Gtk.WindowPosition.MOUSE);
-  Window.connect('destroy', Gtk.main_quit);
-  Window.decorated = false;
-  Window.set_default_size(350, 550);
-
-  scrolled_window = new Gtk.ScrolledWindow();
-
-  //open the website
-  const cookieStorage = prepareCookieStorage();
-  const webContext = Webkit.WebContext.get_default();
-  const cookieManager = webContext.get_cookie_manager();
-  cookieManager.set_persistent_storage(cookieStorage, Webkit.CookiePersistentStorage.SQLITE);
-
-  webView = new Webkit.WebView({ web_context: webContext });
-  scrolled_window.add(webView);
-
-  //load the URL and add it to WebView
-  webView.load_uri('https://chat.openai.com/chat');
-
-  Window.add(scrolled_window)
-  Window.show_all();
-    
-  Gtk.main();
-}
-
-createWindow();
+const [, , x, y] = ARGV;
+createWindow(parseInt(x), parseInt(y));
+Gtk.main();
+log('Script execution completed');
